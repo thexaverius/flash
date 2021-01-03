c1798802c7cface1fcb8a5f97165d4df8ac2f70b
John <flash@bekijkmijeens.nl>
2021-01-03 14:29:33 +0100

    env-file toegevoegd

--- flash/settings.py
+++ flash/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#&!0w4ln-j8)d#ij-14+7ql@2h31)7lrpawgrw)0tj5maioeoj'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
